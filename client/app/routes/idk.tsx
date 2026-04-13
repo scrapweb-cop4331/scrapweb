fc363c8fecbf7e6e267ebbc2c69ff76de4313e8d
thekightkeeper1 <ty.singh828@gmail.com>
2026-04-13 10:29:23 -0400

    idk this is just a test route

--- client/app/routes/idk.tsx
+++ client/app/routes/idk.tsx
@@ -7,7 +7,7 @@ export default function IDKRoute() {
         {/* <video height="320px" src="/app/assets/test.mp4"></video> */}
         <Frame bgColor="$material" w="320px" margin="60px" padding="2px">
 
-        <Audio src="/app/assets/luckystar.mp3"></Audio>
+        <Audio name="test" src="/app/assets/luckystar.mp3"></Audio>
         </Frame>
 
         <Video w="320px" src="/app/assets/test.mp4"></Video>
