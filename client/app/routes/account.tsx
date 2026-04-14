dbf487ff9ffc32c1059ff6f6e4650c12bd57cd5e
thekightkeeper1 <ty.singh828@gmail.com>
2026-04-14 17:58:29 -0400

    Start work on edit user page

---
+++ client/app/routes/account.tsx
@@ -0,0 +1,14 @@
+import { Modal } from "@react95/core";
+
+
+export default function AccountRoute() {
+
+    return (
+            <div style={{display: "flex"}}>
+
+                <Modal>
+                    
+                </Modal>
+            </div>
+    )
+    }
